3b49686d98b74a4204c9fb09d879ac1f391fe260
huzeming <zeming.hzm@alibaba-inc.com>
2020-02-11 22:49:12 +0800

    [ADD] 0337

--- 0337.go
+++ 0337.go
@@ -1,5 +1,26 @@
 package main
 
-func rob(root *TreeNode) int {
+func rob3(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	val1 := root.Val
+
+	val2 := 0
+	if root.Left != nil {
+		val1 += rob3(root.Left.Left) + rob3(root.Left.Right)
+	}
+
+	if root.Right != nil {
+		val1 += rob3(root.Right.Left) + rob3(root.Right.Right)
+	}
+
+	val2 = rob3(root.Left) + rob3(root.Right)
+	if val1 > val2 {
+		return val1
+	} else {
+		return val2
+	}
 
 }
