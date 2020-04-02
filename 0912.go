74bacb29d12f79aa10bb5c5b7c05360bf2ebd0b9
huzeming <huzeming2019@outlook.com>
2020-04-02 23:49:20 +0800

    offer-0010-01 0912-quicksort

--- 0912.go
+++ 0912.go
@@ -1,7 +1,98 @@
 package main
 
 func sortArray(nums []int) []int {
-	return bubble(nums)
+	//return bubble(nums)
+	//return mergeSort(nums)
+	return quickSort(nums)
+}
+
+func quickSort(nums []int) []int {
+	if len(nums) == 1 {
+		return nums
+	}
+
+	quickSortReal(nums, 0, len(nums)-1)
+
+	return nums
+}
+
+func quickSortReal(nums []int, left int, right int) {
+
+	if left < right {
+		index := getIndex(nums, left, right)
+		quickSortReal(nums, left, index-1)
+		quickSortReal(nums, index+1, right)
+	}
+
+}
+
+func getIndex(nums []int, left int, right int) int {
+
+	provt := nums[left]
+
+	for left < right {
+
+		for left < right && nums[right] > provt {
+			right--
+		}
+		if left < right {
+			nums[left] = nums[right]
+			left++
+		}
+
+		for left < right && nums[left] < provt {
+			left++
+		}
+
+		if left < right {
+			nums[right] = nums[left]
+			right--
+		}
+
+	}
+
+	nums[left] = provt
+
+	return left
+
+}
+
+func swap(nums []int, left int, right int) {
+	tmp := nums[left]
+	nums[left] = nums[right]
+	nums[right] = tmp
+}
+
+func mergeSort(nums []int) []int {
+
+	if len(nums) == 1 {
+		return nums
+	}
+
+	leftSortPart := mergeSort(nums[:len(nums)/2])
+	rightSortPart := mergeSort(nums[len(nums)/2:])
+
+	var result []int
+
+	lIndex := 0
+	rIndex := 0
+
+	for lIndex < len(leftSortPart) && rIndex < len(rightSortPart) {
+		if leftSortPart[lIndex] < rightSortPart[rIndex] {
+			result = append(result, leftSortPart[lIndex])
+			lIndex++
+		} else {
+			result = append(result, rightSortPart[rIndex])
+			rIndex++
+		}
+	}
+
+	if lIndex == len(leftSortPart) {
+		return append(result, rightSortPart[rIndex:]...)
+	} else if rIndex == len(rightSortPart) {
+		return append(result, leftSortPart[lIndex:]...)
+	}
+	return result
 }
 
 func bubble(nums []int) []int {
---
+++ offer-0010-01.go
@@ -0,0 +1,28 @@
+package main
+
+func fib(n int) int {
+
+	f := make([]int, 2)
+	f[0] = 0
+	f[1] = 1
+
+	if n < 2 {
+		return f[n]
+	}
+
+	f1 := 0
+	f2 := 1
+	fn := f1 + f2
+
+	for i := 2; i <= n; i++ {
+		fn = f1 + f2
+		if fn > 1000000007 {
+			fn -= 1000000007
+		}
+		f1 = f2
+		f2 = fn
+	}
+
+	return fn
+
+}
